4e1a01d2f65f094b19c78813a18c619ab9abb15d
ehsanmehrjoo <ehsan585147z@gmail.com>
2024-09-19 11:48:15 +0330

    Update

--- src/components/App.js
+++ src/components/App.js
@@ -13,98 +13,98 @@ import Footer from "./Footer";
 import ReviewAnswers from "./ReviewAnswers";
 import { useEffect, useReducer } from "react";
 
-const SECS_PER_QUESTION = 30;
-const initialState = {
-  questions: [],
-  status: "loading",
-  index: 0,
-  answer: null,
-  points: 0,
-  HighScore: 0,
-  secondsRemaining: null,
-};
+// const SECS_PER_QUESTION = 30;
+// const initialState = {
+//   questions: [],
+//   status: "loading",
+//   index: 0,
+//   answer: null,
+//   points: 0,
+//   HighScore: 0,
+//   secondsRemaining: null,
+// };
 
-function reducer(state, action) {
-  switch (action.type) {
-    case "dataReceived":
-      return { ...state, questions: action.payload, status: "ready" };
-    case "start":
-      return {
-        ...state,
-        status: "active",
-        secondsRemaining: state.questions.length * SECS_PER_QUESTION,
-      };
-    case "dataFailed":
-      return { ...state, status: "error" };
-      case "newAnswer": {
-        const updatedQuestions = state.questions.map((question, index) =>
-          index === state.index
-            ? { ...question, answer: action.payload }  // ذخیره پاسخ در سوال فعلی
-            : question
-        );
+// function reducer(state, action) {
+//   switch (action.type) {
+//     case "dataReceived":
+//       return { ...state, questions: action.payload, status: "ready" };
+//     case "start":
+//       return {
+//         ...state,
+//         status: "active",
+//         secondsRemaining: state.questions.length * SECS_PER_QUESTION,
+//       };
+//     case "dataFailed":
+//       return { ...state, status: "error" };
+//       case "newAnswer": {
+//         const updatedQuestions = state.questions.map((question, index) =>
+//           index === state.index
+//             ? { ...question, answer: action.payload }  // ذخیره پاسخ در سوال فعلی
+//             : question
+//         );
       
-        const question = updatedQuestions[state.index];
+//         const question = updatedQuestions[state.index];
         
-        return {
-          ...state,
-          questions: updatedQuestions,
-          points:
-            action.payload === question.correctOption
-              ? state.points + question.points
-              : state.points,
-          answer: action.payload,
-        };
-      }
+//         return {
+//           ...state,
+//           questions: updatedQuestions,
+//           points:
+//             action.payload === question.correctOption
+//               ? state.points + question.points
+//               : state.points,
+//           answer: action.payload,
+//         };
+//       }
       
-    case "nextQuestion":
-      return { ...state, index: state.index + 1, answer: null };
-    case "finished":
-      return {
-        ...state,
-        status: "finished",
-        HighScore:
-          state.points > state.HighScore ? state.points : state.HighScore,
-      };
-    case "restarting":
-      return { ...initialState, questions: state.questions, status: "ready" };
-    case "tick": {
-      const newSecondsRemaining = state.secondsRemaining - 1;
-      return {
-        ...state,
-        secondsRemaining: newSecondsRemaining,
-        status: newSecondsRemaining === 0 ? "finished" : state.status,
-      };
-    }
-    case "ReviewAnswers":
-      return { ...state, status: "review" }; // حالت جدید برای مرور پاسخ‌ها
-    default:
-      throw new Error("Action unknown");
-  }
-}
+//     case "nextQuestion":
+//       return { ...state, index: state.index + 1, answer: null };
+//     case "finished":
+//       return {
+//         ...state,
+//         status: "finished",
+//         HighScore:
+//           state.points > state.HighScore ? state.points : state.HighScore,
+//       };
+//     case "restarting":
+//       return { ...initialState, questions: state.questions, status: "ready" };
+//     case "tick": {
+//       const newSecondsRemaining = state.secondsRemaining - 1;
+//       return {
+//         ...state,
+//         secondsRemaining: newSecondsRemaining,
+//         status: newSecondsRemaining === 0 ? "finished" : state.status,
+//       };
+//     }
+//     case "ReviewAnswers":
+//       return { ...state, status: "review" }; // حالت جدید برای مرور پاسخ‌ها
+//     default:
+//       throw new Error("Action unknown");
+//   }
+// }
 
 export default function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  // const [state, dispatch] = useReducer(reducer, initialState);
 
-  const numQuestion = state.questions?.length || 0;
-  const maxPossiblePoints = state.questions.reduce(
-    (prev, cur) => prev + cur.points,
-    0
-  );
+  // const numQuestion = state.questions?.length || 0;
+  // const maxPossiblePoints = state.questions.reduce(
+  //   (prev, cur) => prev + cur.points,
+  //   0
+  // );
 
-  useEffect(() => {
-    fetch(`http://localhost:9000/questions`)
-      .then((res) => {
-        if (!res.ok) {
-          throw new Error("Network response was not ok");
-        }
-        return res.json();
-      })
-      .then((data) => {
-        dispatch({ type: "dataReceived", payload: data });
-      })
-      .catch((err) => dispatch({ type: "dataFailed", payload: err }));
-  }, []);
-
+  // useEffect(() => {
+  //   fetch(`http://localhost:000/questions`)
+  //     .then((res) => {
+  //       if (!res.ok) {
+  //         throw new Error("Network response was not ok");
+  //       }
+  //       return res.json();
+  //     })
+  //     .then((data) => {
+  //       dispatch({ type: "dataReceived", payload: data });
+  //     })
+  //     .catch((err) => dispatch({ type: "dataFailed", payload: err }));
+  // }, []);
+con
   return (
     <div className="app">
       <Header />
--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "./components/App";
+import { QuizProvider } from "./Context/QuizContext";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
+  <QuizProvider>
     <App />
+  </QuizProvider>
   </StrictMode>
 );
