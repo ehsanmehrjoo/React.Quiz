6726bd4111eebd1ee5a48951d5bcd209d1d7dad8
ehsanmehrjoo <ehsan585147z@gmail.com>
2024-09-19 12:05:52 +0330

    Update FinisheScreen

--- src/components/FinisheScreen.js
+++ src/components/FinisheScreen.js
@@ -1,13 +1,14 @@
+import { useQuiz } from "../Context/QuizContext";
 
- function FinisheScreen ({points, maxPossiblePoints , HighScore , dispatch})  {
-  con
-    const percentage = (points / maxPossiblePoints) * 100;
+ function FinisheScreen ()  {
+  const {maxPossiblePoints , state , dispatch} = useQuiz();
+    const percentage = (state.points / maxPossiblePoints) * 100;
   return (
     <>
     <p className="result">
-    🤔You scored <strong>{points}</strong> out of {maxPossiblePoints} ({Math.ceil(percentage)}%)
+    🤔You scored <strong>{state.points}</strong> out of {maxPossiblePoints} ({Math.ceil(percentage)}%)
     </p>
-    <p className="highscore">(Highscore: {HighScore} points)</p>
+    <p className="highscore">(Highscore: {state.HighScore} points)</p>
 
     <button className="btn btn-ui" type="button" onClick={() => dispatch({type: "restarting"})}>Restart quiz </button>
     <button className="btn " type="button" onClick={() => dispatch({type: "ReviewAnswers"})}>Review the answers </button>
