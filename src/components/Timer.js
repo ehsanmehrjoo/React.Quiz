10de5bdd575805f0e9b97db82344eee6afa44c86
ehsanmehrjoo <ehsan585147z@gmail.com>
2024-09-19 12:41:20 +0330

    Update Timer

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,8 +1,8 @@
 import { useEffect } from "react";
-import { useQuiz } from "../Context/QuizContext";
+import useQuizState from "../hook/useQuizState";
 
  function Timer ()  {
-  const {state , dispatch} = useQuiz();
+  const {state , dispatch} = useQuizState();
 
 const mins = Math.floor(state.secondsRemaining / 60);
 const seconds = state.secondsRemaining % 60;
