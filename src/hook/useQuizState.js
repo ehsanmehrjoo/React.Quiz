a00ce16010a96659389f5f124a324100e7b40455
ehsanmehrjoo <ehsan585147z@gmail.com>
2024-09-19 12:38:16 +0330

    Create a custom hook to consume state all over the application

--- src/hook/useQuizState.js
+++ src/hook/useQuizState.js
@@ -1,5 +1,7 @@
 import { useQuiz } from "../Context/QuizContext";
 
 function useQuizState(){
-    
+    const {  state, dispatch, numQuestion, maxPossiblePoints} = useQuiz()
+    return {state, dispatch, numQuestion, maxPossiblePoints}
 }
+export default useQuizState;
